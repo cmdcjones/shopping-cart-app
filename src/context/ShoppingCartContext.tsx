3ec728db5911dc359d1eac5468329abd5073bf9c
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-11 15:47:31 -0500

    add ShoppingCart component to context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, ReactNode, useState } from "react"
+import { ShoppingCart } from "../components/ShoppingCart"
 
 type ShoppingCartProviderProps = {
   children: ReactNode
@@ -13,6 +14,7 @@ type ShoppingCartContext = {
   closeCart: () => void
   cartQuantity: number
   cartItems: CartItem[]
+  cartTotal: number
 }
 
 type CartItem = {
@@ -91,6 +93,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       }}
     >
       {children}
+      <ShoppingCart isOpen={isOpen}/>
     </ShoppingCartContext.Provider>
   );
 }

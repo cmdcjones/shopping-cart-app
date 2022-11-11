7139826bbe9a95a15197061dd6987582fd6bbd5c
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-10 20:44:31 -0500

    add formatCurrency util function

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import { formatCurrency } from '../utilities/formatCurrency'
 
 type StoreItemProps = {
   id: number,
@@ -25,7 +26,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                         mb-4"
           >
             <span className="fs-2">{name}</span>
-            <span className="ms-2 text-muted">{price}</span>
+            <span className="ms-2 text-muted">{formatCurrency(price)}</span>
           </Card.Title>
         </Card.Body>
       </Card>
